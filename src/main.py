d78ae497128500f7128cbec7ab07a18b647ebfb4
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-06-24 18:04:29 +0100

    adjusted test code

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
-age = 36
-greeting = f"Hello, I am Ben and I am {age}"
+# Testing the int function in Python to convert a string to an integer
+age = int("36")
+
+greeting = f"Hello, I am Ben and I am {age} years old"
 print(greeting)
+
+# This is a comment in python
