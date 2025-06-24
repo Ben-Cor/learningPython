50bbdbae992edb2514c938358330bc33cd9089b0
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-06-24 18:20:43 +0100

    added condition tests to code

--- src/main.py
+++ src/main.py
@@ -9,3 +9,11 @@ print(greeting)
 for i in range(5):
     print(5-i)
 print("Blast Off!")
+
+
+if age > 30:
+    print("You are over 30 years old")
+elif age > 18:
+    print("You are over 18 years old")
+else:
+    print("You are under 18 years old")
