b8e1156c9598bfee3bdc89fa2ae3ca95eb3f6e00
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-06-30 19:35:58 +0100

    lists practice continued

--- src/main.py
+++ src/main.py
@@ -116,3 +116,36 @@ print(squares)
 
 cubes = [digit ** 3 for digit in single_digits]
 print(cubes)
+
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+# calculate the total price
+total_price=0
+for price in prices:
+  total_price += price
+
+# calculate the average price
+average_price = total_price / len(prices)
+print(f"Average Haircut Price: {average_price:.2f}")
+
+#form new prices at 5 less than prices
+new_prices = [price - 5 for price in prices]
+print(new_prices)
+
+# calculate total revenue
+total_revenue = 0
+for i in range(len(hairstyles)):
+  total_revenue += (prices[i] * last_week[i])
+print(f"Total Revenue: {total_revenue}")
+
+#average daily revenue
+average_daily_revenue = total_revenue/7
+print(average_daily_revenue)
+
+#cuts under 30 price
+cuts_under_30 = [hairstyles[i] for i in range(len(new_prices)) if new_prices[i] < 30]
+print(cuts_under_30)
