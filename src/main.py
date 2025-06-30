6665fdd34054da4b3cfa29e54b22a72f0e31e1a6
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-06-30 11:16:10 +0100

    lists practice

--- src/main.py
+++ src/main.py
@@ -79,7 +79,7 @@ print("Premium cost (Ground) = $125")
 
 #Drone Shipping
 
-dront_cost = 0
+drone_cost = 0
 if weight <= 2:
   drone_cost = (weight * 4.5)
 elif weight <= 6:
@@ -90,3 +90,16 @@ else:
   drone_cost = (weight * 14.25)
 
 print(f"Total cost (Drone) = ${drone_cost:.2f}")
+
+# list practice
+inventory = ["twin bed", "twin bed", "headboard", "queen bed", "king bed", "dresser", "dresser", "table", "table", "nightstand", "nightstand", "king bed", "king bed", "twin bed", "twin bed", "sheets", "sheets", "pillow", "pillow"]
+
+inventory_len = len(inventory)
+first = inventory[0]
+last = inventory[-1]
+inventory_2_6 = inventory[2:6]
+first_3 = inventory[:3]
+twin_beds = inventory.count("twin bed")
+removed_item = inventory.pop(4)
+inventory.insert(10, "19th Century Bed Frame")
+inventory.sort()
