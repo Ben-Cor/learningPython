8d59d2a1b7446027c96dd8a947f073ba248f3515
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-06-30 12:03:47 +0100

    list zip practice

--- src/main.py
+++ src/main.py
@@ -127,3 +127,12 @@ pizza_and_prices.insert(4, [2.5, "peppers"])
 three_cheapest = pizza_and_prices[:3]
 
 print(three_cheapest)
+
+#zip practice
+owners = ["Jenny", "Alexus", "Sam", "Grace"]
+dogs_names = ["Elphonse", "Dr. Doggy DDS", "Carter", "Ralph"]
+
+names_and_dogs_names = zip(owners, dogs_names)
+list_of_names_and_dogs_names = list(names_and_dogs_names)
+
+print(list_of_names_and_dogs_names)
