f6bc91fe20773df9343dbe367430a355b494a59f
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-06-30 15:56:36 +0100

    comprension practice

--- src/main.py
+++ src/main.py
@@ -98,3 +98,8 @@ for age in ages:
   if age < 21:
     continue
   print(age)
+
+# comprehension practice
+grades = [90, 88, 62, 76, 74, 89, 48, 57]
+scaled_grades = [grade + 10 for grade in grades]
+print(scaled_grades)
