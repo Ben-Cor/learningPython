2e4ec78539046e0bd1768f7cdb05aa43786485fa
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-06-30 13:29:48 +0100

    loop practice expanded

--- src/main.py
+++ src/main.py
@@ -155,3 +155,10 @@ for dog_breed in dog_breeds_available_for_adoption:
   if dog_breed == dog_breed_I_want:
     print("They have the dog I want!")
     break
+
+ages = [12, 38, 34, 26, 21, 19, 67, 41, 17]
+
+for age in ages:
+  if age < 21:
+    continue
+  print(age)
