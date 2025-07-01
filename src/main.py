4c0425322b608d154d7ca4c8e025735a86d7fea0
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-01 11:10:22 +0100

    further functions practice

--- src/main.py
+++ src/main.py
@@ -126,3 +126,21 @@ print(min_price)
 
 rounded_price = round(tshirt_price, 1)
 print(rounded_price)
+
+#function further practice
+current_budget = 3500.75
+
+def print_remaining_budget(budget):
+  print("Your remaining budget is: $" + str(budget))
+
+print_remaining_budget(current_budget)
+
+# Write your code below:
+
+def deduct_expense(budget, expense):
+  return budget - expense
+
+shirt_expense = 9
+new_budget_after_shirt = deduct_expense(current_budget, shirt_expense)
+
+print_remaining_budget(new_budget_after_shirt)
