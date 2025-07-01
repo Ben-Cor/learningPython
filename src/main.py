f33076f38fcd434a1ce25c2b67da67f81c3ffcc0
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-01 10:49:15 +0100

    built in function practice

--- src/main.py
+++ src/main.py
@@ -109,3 +109,20 @@ trip_planner("France", "Germany", "Denmark")
 trip_planner("Denmark", "France", "Germany")
 trip_planner(first_destination = "Iceland", final_destination = "Germany", second_destination = "India")
 trip_planner("Brooklyn", "Queens")
+
+#built in functions practice
+tshirt_price = 9.75
+shorts_price = 15.50
+mug_price = 5.99
+poster_price = 2.00
+
+max_price = max(tshirt_price, shorts_price, mug_price, poster_price)
+
+print(max_price)
+
+min_price = min(tshirt_price, shorts_price, mug_price, poster_price)
+
+print(min_price)
+
+rounded_price = round(tshirt_price, 1)
+print(rounded_price)
