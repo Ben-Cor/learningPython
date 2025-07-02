7b535ca554758486d50f2f0b2c79247bc94f722c
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-02 09:16:07 +0100

    list based function practice

--- src/main.py
+++ src/main.py
@@ -113,3 +113,10 @@ else:
 
 print("Total: " + str(total))
 print("Is it over budget? " + str(over_budget))
+
+
+def append_size(my_list):
+  my_list.append(len(my_list))
+  return my_list
+
+print(append_size([23, 42, 108]))
