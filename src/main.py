9d7f5dba57d985e366e780b576ddfc5fc8c62435
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-02 11:03:30 +0100

    loops practice

--- src/main.py
+++ src/main.py
@@ -117,3 +117,12 @@ def delete_starting_evens(my_list):
 
 print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
 print(delete_starting_evens([4, 8, 10]))
+
+def odd_indices(my_list):
+  new_list = []
+  for i in range(len(my_list)):
+    if i % 2 != 0:
+      new_list.append(my_list[i])
+  return new_list
+
+print(odd_indices([4, 3, 7, 10, 11, -2]))
