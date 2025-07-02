cab3f987be86adf34daec4b5c65ff58a8fcda27e
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-02 12:14:15 +0100

    loops practice

--- src/main.py
+++ src/main.py
@@ -117,3 +117,14 @@ def same_values(lst1, lst2):
   return matching_list
 
 print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
+
+
+def reversed_list(lst1, lst2):
+  reverse = True
+  for i in range(len(lst1)):
+    if lst1[i] != lst2[len(lst2) - 1 - i]:
+      reverse = False
+  return reverse
+
+print(reversed_list([1, 2, 3], [3, 2, 1]))
+print(reversed_list([1, 5, 3], [3, 2, 1]))
