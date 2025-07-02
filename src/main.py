07212d6f0948d738574590335ef0b3081262c054
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-02 09:24:48 +0100

    list based function practice

--- src/main.py
+++ src/main.py
@@ -129,3 +129,11 @@ def append_sum(my_list):
   return my_list
 
 print(append_sum([1, 1, 2]))
+
+def larger_list(my_list1, my_list2):
+  if len(my_list2) > len(my_list1):
+    return my_list2[-1]
+  else:
+    return my_list1[-1]
+
+print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
