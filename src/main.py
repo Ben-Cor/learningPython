afe9dbe8c97bce8d22787e26278e05d60800f3bd
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-03 14:12:45 +0100

    math challange continued

--- src/main.py
+++ src/main.py
@@ -117,3 +117,11 @@ print(win_percentage(5, 5))
 # should print 50
 print(win_percentage(10, 0))
 # should print 100
+
+def average(num1, num2):
+  return (num1 + num2)/2
+
+print(average(1, 100))
+# The average of 1 and 100 is 50.5
+print(average(1, -1))
+# The average of 1 and -1 is 0
