f0d274979a1c7f3e7945affbb7e9476ed9e70858
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-03 13:19:34 +0100

    more practice

--- src/main.py
+++ src/main.py
@@ -92,3 +92,11 @@
 # print(f"Total cost (Drone) = ${drone_cost:.2f}")
 
 # loops practice
+def tenth_power(num):
+  return num ** 10
+
+print(tenth_power(1))
+# 1 to the 10th power is 1
+print(tenth_power(0))
+# 0 to the 10th power is 0
+print(tenth_power(2))
