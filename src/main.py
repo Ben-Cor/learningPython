3869a021e3b893dcc4385d6f584e80764fc78d64
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-07 12:43:27 +0100

    strings practice

--- src/main.py
+++ src/main.py
@@ -91,63 +91,6 @@
 #
 # print(f"Total cost (Drone) = ${drone_cost:.2f}")
 
-# loops practice
-def tenth_power(num):
-  return num ** 10
-
-print(tenth_power(1))
-# 1 to the 10th power is 1
-print(tenth_power(0))
-# 0 to the 10th power is 0
-print(tenth_power(2))
-
-def square_root(num):
-  return num ** 0.5
-
-print(square_root(16))
-# should print 4
-print(square_root(100))
-# should print 10
-
-def win_percentage(wins, losses):
-  games = wins + losses
-  return wins / (games/100)
-
-print(win_percentage(5, 5))
-# should print 50
-print(win_percentage(10, 0))
-# should print 100
-
-def average(num1, num2):
-  return (num1 + num2)/2
-
-print(average(1, 100))
-# The average of 1 and 100 is 50.5
-print(average(1, -1))
-# The average of 1 and -1 is 0
-
-def remainder(num1, num2):
-  return (num1 * 2) % (0.5 * num2)
-
-print(remainder(15, 14))
-# should print 2
-print(remainder(9, 6))
-# should print 0
-
-def first_three_multiples(num):
-  for i in range(1, 4):
-    print(num*i)
-  return num*3
-
-first_three_multiples(10)
-# should print 10, 20, 30, and return 30
-first_three_multiples(0)
-# should print 0, 0, 0, and return 0
-
-def tip(total, percentage):
-  return total * (percentage/100)
-
-print(tip(10, 25))
-# should print 2.5
-print(tip(0, 100))
-# should print 0.0
+# strings practice
+my_name = "Ben"
+first_initial = my_name[0]
