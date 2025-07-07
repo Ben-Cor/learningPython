221e5df7621f511f90db4bc486a41d46e809d62c
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-07 14:58:52 +0100

    strings practice

--- src/main.py
+++ src/main.py
@@ -131,3 +131,18 @@ def letter_check(word, letter):
       output = True
   return output
 
+
+def contains(big_string, little_string):
+  if little_string in big_string:
+    return True
+  else:
+    return False
+
+
+def common_letters(string_one, string_two):
+  common = []
+  for letter in string_one:
+    if (letter in string_two) and not (letter in common):
+      common.append(letter)
+  return common
+
