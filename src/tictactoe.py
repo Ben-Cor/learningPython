e3ecc8cce934cc7ec2512cd8b9900e1697315809
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-08 11:57:53 +0100

    tic tac toe intro message

---
+++ src/tictactoe.py
@@ -0,0 +1,7 @@
+# This is a simple Tic Tac Toe game implementation in Python.
+
+# Introductory message
+print("Welcome to the Tic Tac Toe game!")
+print("This is a two-player game where you will take turns to place your marks on the board.")
+
+
