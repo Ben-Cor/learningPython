4f650f870e031b920a1f1ccb2755e601ca2a9169
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-08 12:52:53 +0100

    tic tac toe board comments and adjustments

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -15,4 +15,10 @@ def display_board(board):
 # creates 3 empty strings using _ as a throwaway variable, creates 3 lots of these strings in a list comprehension
 display_board([[" " for _ in range(3)] for _ in range(3)])
 
-input("Player 1 (X) - Which column do you want to play in? (1-3)?")
+player_choice = input("Player 1 (X) - Which column do you want to play in? (1-3)?")
+    if player_choice == "1":
+        print("You chose column 1")
+    elif player_choice == "2":
+        print("You chose column 2")
+    elif player_choice == "3":
+        print("You chose column 3")
