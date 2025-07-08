18ebe44ccb06294bf24f85a807e01e820baa1311
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-08 10:03:42 +0100

    string methods

--- src/main.py
+++ src/main.py
@@ -104,7 +104,10 @@ for name in author_names:
 
 print(author_last_names)
 
-reapers_line_one_words = ["Black", "reapers", "with", "the", "sound", "of"
-													, "steel", "on", "stones"]
+reapers_line_one_words = ["Black", "reapers", "with", "the", "sound", "of", "steel", "on", "stones"]
 
 reapers_line_one = ' '.join(reapers_line_one_words)
+
+winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among', 'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
+
+winter_trees_full = "\n".join(winter_trees_lines)
