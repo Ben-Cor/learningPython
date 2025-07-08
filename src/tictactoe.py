1035c2d90ce4d7f036ee720920a6e7a278570aa2
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-08 12:43:14 +0100

    tic tac toe board comments and adjustments

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -9,9 +9,10 @@ def display_board(board):
     print("\nCurrent board:")
     for row in board:
         print(" | ".join(row)) # Join each row with ' | ' for better readability
-        print("-" * 13) # Separator line for better readability
+        print("-" * 9) # Separator line for better readability
 
 # Function to display board called at the start
-display_board([[" " for _ in range(4)] for _ in range(4)])
+# creates 3 empty strings using _ as a throwaway variable, creates 3 lots of these strings in a list comprehension
+display_board([[" " for _ in range(3)] for _ in range(3)])
 
-input("Which column do you want to play in? (1-4)?")
+input("Player 1 (X) - Which column do you want to play in? (1-3)?")
