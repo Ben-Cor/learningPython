7888d36dba5823ef22292fe5ee1a569148c216fe
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-08 09:34:06 +0100

    string methods

--- src/main.py
+++ src/main.py
@@ -91,3 +91,15 @@
 #
 # print(f"Total cost (Drone) = ${drone_cost:.2f}")
 
+# string methods
+authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
+
+author_names = authors.split(',')
+
+print(author_names)
+
+author_last_names = []
+for name in author_names:
+  author_last_names.append(name.split()[-1])
+
+print(author_last_names)
