f6ec5cf8cd5ace1e6f2e016bf7a8d96a1361b0cb
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-09 11:46:51 +0100

    strings practice

--- src/main.py
+++ src/main.py
@@ -141,3 +141,16 @@ def poem_description(publishing_date, author, title, original_work):
   return poem_desc
 
 my_beard_description = poem_description("1974", "Shel Silverstein", "My Beard", "Where the Sidewalk Ends")
+
+highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
+
+print(highlighted_poems)
+
+highlighted_poems_list = highlighted_poems.split(",")
+print(highlighted_poems_list)
+
+highlighted_poems_stripped = []
+for poem in highlighted_poems_list:
+  highlighted_poems_stripped.append(poem.strip())
+
+print(highlighted_poems_stripped)
