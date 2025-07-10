1fb2e48ecf2c030ffe3b5338d41482259a4813ec
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-10 11:11:33 +0100

    revised code for decrypt to remove alphabet as param

--- src/main.py
+++ src/main.py
@@ -44,10 +44,11 @@
 #
 # print(f"{name}, you asked - {question}. The 8 ball says: {answer}")
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+
 message = "xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!"
 
-def decoding(alphabet, message, offset):
+def decoding(message, offset):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
     output = ""
     for i in range (len(message)):
         if message[i] in alphabet:
@@ -57,10 +58,10 @@ def decoding(alphabet, message, offset):
             output += message[i]
     return output
 
-print(decoding(alphabet, message, 10))
+print(decoding(message, 10))
 
 reply = "Hello, world! This is a test message to see if the decoding works correctly."
-print(decoding(alphabet, reply, 10))
+print(decoding(reply, 10))
 
 response = "jxu evviuj veh jxu iusedt cuiiqwu yi vekhjuud."
-print(decoding(alphabet, response, 10))
+print(decoding(response, 10))
