1aadac553879d22e7ecac8dda9822daa22ab76c8
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-10 11:10:53 +0100

    revised code for decrypt to make variable offset

--- src/main.py
+++ src/main.py
@@ -47,14 +47,20 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 message = "xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!"
 
-def decoding(alphabet, message):
+def decoding(alphabet, message, offset):
     output = ""
     for i in range (len(message)):
         if message[i] in alphabet:
             index = alphabet.find(message[i])
-            output += alphabet[(index + 10) % 26] # Wrap around using modulo 26
+            output += alphabet[(index + offset) % 26] # Wrap around using modulo 26
         else:
             output += message[i]
     return output
 
-print(decoding(alphabet, message))
+print(decoding(alphabet, message, 10))
+
+reply = "Hello, world! This is a test message to see if the decoding works correctly."
+print(decoding(alphabet, reply, 10))
+
+response = "jxu evviuj veh jxu iusedt cuiiqwu yi vekhjuud."
+print(decoding(alphabet, response, 10))
