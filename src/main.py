cafb81ee58514910fea9e1863e169f199733c382
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-11 11:44:45 +0100

    practice code cleanup

--- src/main.py
+++ src/main.py
@@ -1,18 +1,2 @@
 # Testing modules
 
-from datetime import datetime
-
-current_time = datetime.now()
-print(current_time)
-
-# Import random below:
-import random
-
-# Create random_list below:
-random_list = []
-
-# Create randomer_number below:
-random_list = [random.randint(1, 100) for i in range(101)]
-randomer_number = random.choice(random_list)
-# Print randomer_number below:
-print(randomer_number)
