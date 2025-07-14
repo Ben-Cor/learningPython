dd4f4e2b944197fccc88118cf8f6fcac3933904f
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-14 16:26:20 +0100

    module practices

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 # Testing modules
+#
+# import codecademylib3_seaborn
+# from matplotlib import pyplot as plt
+# import random
+#
+# numbers_a = range(1,13)
+# numbers_b = random.sample((range(0,1001)), 12)
+# plt.plot(numbers_a, numbers_b)
+# plt.show()
 
-import codecademylib3_seaborn
-from matplotlib import pyplot as plt
-import random
+# Import Decimal below:
+from decimal import Decimal
 
-numbers_a = range(1,13)
-numbers_b = random.sample((range(0,1001)), 12)
-plt.plot(numbers_a, numbers_b)
-plt.show()
+# Fix the floating point math below:
+two_decimal_points = Decimal('0.2') + Decimal('0.69')
+print(two_decimal_points)
+
+four_decimal_points = Decimal('0.53') * Decimal('0.65')
+print(four_decimal_points)
