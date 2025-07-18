c4621842bdf6d743c7a958f3fac9b82a141ddd93
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-18 13:25:25 +0100

    custom module and imports added

---
+++ src/custom_module.py
@@ -0,0 +1,3 @@
+def generate_time_travel_message(year, destination, cost):
+  return f"You travelled to {destination} in the year {year} at a cost of {cost}"
+
---
+++ src/main.py
@@ -0,0 +1,7 @@
+import datetime as dt
+from decimal import Decimal
+from random import randint, choice
+import custom_module
+
+datetime = dt.datetime.now()
+print(datetime)
