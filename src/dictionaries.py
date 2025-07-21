41db53d975ce04c1e7ec09643ba04e43478f5832
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-21 11:36:37 +0100

    dictionaries practice

--- src/dictionaries.py
+++ src/dictionaries.py
@@ -5,3 +5,8 @@ animals_in_zoo["zebras"]=8
 animals_in_zoo["monkeys"]=12
 animals_in_zoo["dinosaurs"]=0
 print(animals_in_zoo)
+
+#adding multiple values at once with update
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+user_ids.update({"theLooper": 138475, "stringQueen": 85739})
+print(user_ids)
