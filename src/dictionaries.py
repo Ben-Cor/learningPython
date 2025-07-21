1dbbcb78de12de604ac9b2cc472c9c2143d80f6f
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-21 11:24:54 +0100

    dictionaries practice

--- src/dictionaries.py
+++ src/dictionaries.py
@@ -1,1 +1,7 @@
 translations = {"mountain": "orod", "bread": "bass", "friend": "mellon", "horse": "roch"}
+
+animals_in_zoo = {}
+animals_in_zoo["zebras"]=8
+animals_in_zoo["monkeys"]=12
+animals_in_zoo["dinosaurs"]=0
+print(animals_in_zoo)
