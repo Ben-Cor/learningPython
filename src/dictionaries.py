92fe340ad132e329862e2b73ff3134fabd5c8f1d
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-21 11:41:45 +0100

    dictionaries practice

--- src/dictionaries.py
+++ src/dictionaries.py
@@ -10,3 +10,8 @@ print(animals_in_zoo)
 user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
 user_ids.update({"theLooper": 138475, "stringQueen": 85739})
 print(user_ids)
+
+#overwriting values
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+oscar_winners["Supporting Actress"]="Viola Davis"
+oscar_winners["Best Picture"]="Moonlight"
