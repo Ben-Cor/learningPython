e125a8873074f28258ec945f2b3ff5d48d3e350f
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-21 10:01:51 +0100

    adjusted function and date/time collection

--- src/main.py
+++ src/main.py
@@ -3,5 +3,11 @@ from decimal import Decimal
 from random import randint, choice
 import custom_module
 
-datetime = dt.datetime.now()
-print(datetime)
+current_year = dt.datetime.now().year
+current_time = dt.datetime.now().time
+print(current_year)
+
+def cost_calculate(target_year):
+  return Decimal(0.55) * (current_year - target_year)
+
+print(cost_calculate(1965))
